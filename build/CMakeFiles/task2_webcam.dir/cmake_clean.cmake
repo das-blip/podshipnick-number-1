518a106bc06b0db0f42f493bc98b36b91a105c3d
das-blip <dasimpressum@gmail.com>
2026-02-18 19:51:35 +0300

    Add files via upload

---
+++ build/CMakeFiles/task2_webcam.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/task2_webcam.dir/task2_webcam.cpp.o"
+  "CMakeFiles/task2_webcam.dir/task2_webcam.cpp.o.d"
+  "task2_webcam"
+  "task2_webcam.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/task2_webcam.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
